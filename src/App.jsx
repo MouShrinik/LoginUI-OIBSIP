359c675227077f9f21470925b6e73560d4e74155
MouShrinik <majumdarshreya71@gmail.com>
2023-12-05 17:19:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,137 @@
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const initialValues = { username: "", email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors, formValues, isSubmit]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "Username is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
+    }
+    return errors;
+  };
+
+
+
+  return (
+    <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 align-middle">
+  
+      <div className="px-6 py-4">
+        <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">
+          Welcome Back
+        </h3>
+
+        <p className="mt-1 text-center text-gray-500 dark:text-gray-400">
+          Login or create account
+        </p>
+
+        <form onSubmit={handleSubmit}>
+          <div className="w-full mt-4">
+            <input
+              className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={formValues.username}
+              onChange={handleChange}
+              aria-label="Username"
+            />
+          </div>
+          <p>{formErrors.username}</p>
+
+          <div className="w-full mt-4">
+            <input
+              className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={formValues.email}
+              onChange={handleChange}
+              aria-label="Email Address"
+            />
+          </div>
+          <p>{formErrors.email}</p>
+
+          <div className="w-full mt-4">
+            <input
+              className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formValues.password}
+              onChange={handleChange}
+              aria-label="Password"
+            />
+          </div>
+          <p>{formErrors.password}</p>
+
+          <div className="flex items-center justify-between mt-4">
+            <a
+              href="#"
+              className="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500"
+            >
+              Forget Password?
+            </a>
+
+            <button
+              className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 
+              transform bg-blue-500 rounded-lg 
+            hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+        <span className="text-sm text-gray-600 dark:text-gray-200">
+          Don`t have an account?{" "}
+        </span>
+
+        <a
+          href="#"
+          className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline"
+        >
+          Register
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
